26099674760d32fead99eea6b8a607946b852e6f
Bruno Ribeiro <bruno.ribeiro.lab@gmail.com>
2025-12-02 19:11:22 -0300

    Add: Footer Component

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+
+export default function Footer() {
+    return (
+        <footer className="page-footer">
+            <p className="mb-0">Copyright © {new Date().getFullYear()}. All right reserved.</p>
+        </footer>
+    );
+}
